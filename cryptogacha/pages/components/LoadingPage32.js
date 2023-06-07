5e0f9ef86d71cfda0a8b4f0eb567c816e4ff8caa
mounir.zaouiti <mounir-2002@live.fr>
2023-06-07 23:11:15 +0200

    gacha coin func random & loading page + style begin

---
+++ cryptogacha/pages/components/LoadingPage32.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "../../styles/loading.module.css";
+
+export default function LoadingPage1(props) {
+    return (
+        <div>LoadingPage</div>
+    )
+}
